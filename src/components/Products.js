348f1a78a8e50ddffa9453ad6bd13c32d64a1b87
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2021-01-06 11:47:09 -0500

    removed props from Product Component in App.js, imported useContext from React and ProductContext from contexts to Products.js, used useContext hook to pull value object properties from ProductContext, refactored Products.js to use values from Context API instead of from passed props

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProductContext } from '../contexts/ProductContext';
 
 // Components
 import Product from './Product';
 
-const Products = props => {
+const Products = () => {
+	//useContext returns the values from the value prop in the ProductContext.Provider
+	const { products, addItem } = useContext(ProductContext);
+
 	return (
+
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
